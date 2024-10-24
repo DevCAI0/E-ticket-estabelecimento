76ebbfec819d1bf8a94eba1897ec5a7c45e1e6d6
Caio Ribeiro <caioribeiro6789@gmail.com>
2024-10-24 09:02:01 -0300

    testando melhorias para instalaçao do app no mobile

--- src/pages/app/dashboard/home.tsx
+++ src/pages/app/dashboard/home.tsx
@@ -1,9 +1,11 @@
 import CardList from "@/components/cards/card-list"
+import InstallPWAButton from "@/components/InstallPWAButton"
 
 export const Home = () => {
   return (
     <div>
         <CardList/>
+        <InstallPWAButton />
     </div>
   )
 }
